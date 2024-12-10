90c84bad0c5c38f6d62b5efe8ac233f09a096fca
HaMyoungHoon <basicouts@gmail.com>
2024-12-10 16:49:05 +0900

    const string 추가

--- src/app/guards/f-extensions.ts
+++ src/app/guards/f-extensions.ts
@@ -2,6 +2,7 @@ import {stringToUserStatus, UserStatus} from "../models/rest/user-status";
 import {Table} from "primeng/table";
 import {SortEvent} from "primeng/api";
 import {RestResult} from "../models/common/rest-result";
+import * as FContentsType from "./f-contents-type";
 
 export function dToMon(date: Date): string {
   let ret = date.getMonth() + 1;
@@ -22,6 +23,11 @@ export function dToMin(date: Date): string {
 export function dToS(date: Date): string {
   let ret = date.getSeconds();
   return ret >= 10 ? `${ret}` : `0${ret}`;
+}
+export function currentDateYYYYMMdd(): string {
+  const date = new Date();
+
+  return `${date.getFullYear()}${dToMon(date)}${dToD(date)}`;
 }
 export function dateToYearFullString(date?: Date): string {
   if (date == null) {
@@ -76,7 +82,7 @@ export async function tryCatchAsync<T>(fn: () => Promise<T>, onError?: (e: any) 
     return await fn();
   } catch (e: any) {
     if (onError) {
-      onError(e);
+      onError(e.message);
     }
     return null;
   }
@@ -145,4 +151,114 @@ export function ellipsis(data?: string, length: number = 20): string {
   }
 
   return data;
+}
+
+export function getFilenameExt(filename: string): string {
+  const dotIndex = filename.indexOf(".") + 1;
+  if (dotIndex <= 0 || dotIndex >= filename.length) {
+    return "";
+  }
+  return filename.substring(dotIndex).toLowerCase();
+}
+export async function getFileExt(file: File, byteCount: number = 8): Promise<string> {
+  const magicNumber = await getMagicNumber(file, byteCount);
+  if (magicNumber.startsWith("50 4B 03 04")) return "zip";
+  if (magicNumber.startsWith("50 4B 30 30 50 4B 03 04")) return "zip";
+  if (magicNumber.startsWith("25 50 44 46")) return "pdf";
+  if (magicNumber.startsWith("FF D8 FF")) return "jpeg";
+  if (magicNumber.startsWith("89 50 4E 47")) return "png";
+  if (magicNumber.startsWith("42 4D")) return "bmp";
+  if (magicNumber.startsWith("52 49 46 46") && (await getMagicNumber(file, 12)).includes("57 45 42 50")) return "webp";
+  if (magicNumber.startsWith("66 74 79 70 68 65 69 63")) return "heic";
+
+  return "unknown";
+}
+export async function getMagicNumber(file: File, byteCount: number = 8): Promise<string> {
+  let header = "";
+  try {
+    const arrayBuff = await file.arrayBuffer();
+    const uint8Array = new Uint8Array(arrayBuff.slice(0, byteCount))
+    uint8Array.forEach(x => header += `${x.toString(16).padStart(2, "0")} `);
+  } catch {
+  }
+  return header.toUpperCase();
+}
+export function isImage(ext: string): boolean {
+  if (ext == "jpeg") return true;
+  if (ext == "jpg") return true;
+  if (ext == "png") return true;
+  if (ext == "bmp") return true;
+  if (ext == "webp") return true;
+  if (ext == "heic") return true;
+
+  return false;
+}
+export function getMimeTypeExt(ext: string): string {
+  switch (ext) {
+    case "aac": return FContentsType.type_aac;
+    case "abw": return FContentsType.type_abw;
+    case "arc": return FContentsType.type_arc;
+    case "avi": return FContentsType.type_avi;
+    case "azw": return FContentsType.type_azw;
+    case "bin": return FContentsType.type_bin;
+    case "bz": return FContentsType.type_bz;
+    case "bz2": return FContentsType.type_bz2;
+    case "csh": return FContentsType.type_csh;
+    case "css": return FContentsType.type_css;
+    case "csv": return FContentsType.type_csv;
+    case "doc": return FContentsType.type_doc;
+    case "epub": return FContentsType.type_epub;
+    case "gif": return FContentsType.type_gif;
+    case "htm": return FContentsType.type_htm;
+    case "html": return FContentsType.type_html;
+    case "heic": return FContentsType.type_heic;
+    case "heif": return FContentsType.type_heif;
+    case "ico": return FContentsType.type_ico;
+    case "ics": return FContentsType.type_ics;
+    case "jar": return FContentsType.type_jar;
+    case "jpeg": return FContentsType.type_jpeg;
+    case "jpg": return FContentsType.type_jpg;
+    case "js": return FContentsType.type_js;
+    case "json": return FContentsType.type_json;
+    case "mid": return FContentsType.type_mid;
+    case "midi": return FContentsType.type_midi;
+    case "mpeg": return FContentsType.type_mpeg;
+    case "mpkg": return FContentsType.type_mpkg;
+    case "odp": return FContentsType.type_odp;
+    case "ods": return FContentsType.type_ods;
+    case "odt": return FContentsType.type_odt;
+    case "oga": return FContentsType.type_oga;
+    case "ogv": return FContentsType.type_ogv;
+    case "ogx": return FContentsType.type_ogx;
+    case "png": return FContentsType.type_png;
+    case "pdf": return FContentsType.type_pdf;
+    case "ppt": return FContentsType.type_ppt;
+    case "rar": return FContentsType.type_rar;
+    case "rtf": return FContentsType.type_rtf;
+    case "sh": return FContentsType.type_sh;
+    case "svg": return FContentsType.type_svg;
+    case "swf": return FContentsType.type_swf;
+    case "tar": return FContentsType.type_tar;
+    case "tif": return FContentsType.type_tif;
+    case "tiff": return FContentsType.type_tiff;
+    case "ttf": return FContentsType.type_ttf;
+    case "txt": return FContentsType.type_txt;
+    case "vsd": return FContentsType.type_vsd;
+    case "wav": return FContentsType.type_wav;
+    case "weba": return FContentsType.type_weba;
+    case "webm": return FContentsType.type_webm;
+    case "webp": return FContentsType.type_webp;
+    case "woff": return FContentsType.type_woff;
+    case "xhtml": return FContentsType.type_xhtml;
+    case "xls": return FContentsType.type_xls;
+    case "xlsx": return FContentsType.type_xlsx;
+    case "xlsm": return FContentsType.type_xlsm;
+    case "xml": return FContentsType.type_xml;
+    case "xul": return FContentsType.type_xul;
+    case "zip": return FContentsType.type_zip;
+    case "3gp": return FContentsType.type_3gp;
+    case "3g2": return FContentsType.type_3g2;
+    case "7z": return FContentsType.type_7z;
+    default: return "";
+  }
 }
