996f46d01bf4a810bb23a498a8769f51164bd5d8
HaMyoungHoon <basicouts@gmail.com>
2024-12-10 16:51:17 +0900

    azure upload service 추가

---
+++ src/app/services/rest/azure-blob.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import {BlobHTTPHeaders, BlobServiceClient, BlockBlobUploadResponse} from "@azure/storage-blob";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AzureBlobService {
+  private blobUrl = "https://mhhablob1.blob.core.windows.net";
+  private containerName = "mhhablob1";
+
+  putUpload(file: File, blobName: string, sasKey: string, mimeType: string, onProgress?: (log: any) => void): Promise<BlockBlobUploadResponse> {
+    const blobServiceClient = new BlobServiceClient(`${this.blobUrl}?${sasKey}`);
+    const containerClient = blobServiceClient.getContainerClient(this.containerName);
+    const blobClient = containerClient.getBlockBlobClient(blobName);
+    blobClient.setHTTPHeaders({
+      blobContentType: mimeType
+    }).then();
+    return blobClient.upload(file, file.size, {
+      onProgress: (log) => onProgress?.(log)
+    });
+  }
+}
