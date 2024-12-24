2e3c006e7e5da3f1722253ac651f202f8c507b5f
HaMyoungHoon <basicouts@gmail.com>
2024-12-24 15:12:25 +0900

    menu 추가

--- src/app/models/common/main-menu-item.ts
+++ src/app/models/common/main-menu-item.ts
@@ -3,10 +3,12 @@ import {MenuItem1} from "./menu-items/menu-item-1";
 import {MenuItem2} from "./menu-items/menu-item-2";
 import {MenuItem3} from "./menu-items/menu-item-3";
 import * as FConstants from "../../guards/f-constants";
+import {MenuItem4} from "./menu-items/menu-item-4";
 
 export function MainMenuItem(): MenuItem[] {
   const ret: MenuItem[] = [];
   ret.push(MenuItem1());
+  ret.push(MenuItem4());
   ret.push(MenuItem2());
   ret.push(MenuItem3());
   return ret;
---
+++ src/app/models/common/menu-items/menu-item-4.ts
@@ -0,0 +1,33 @@
+import {MenuItem} from "primeng/api";
+import * as FConstants from "../../../guards/f-constants";
+
+export function MenuItem4(): MenuItem {
+  return {
+    label: "menu-4.title",
+    visible: true,
+    styleClass: "top-menu",
+    items: [
+      {
+        label: "menu-4.sub-menu.sub-1",
+        visible: true,
+        icon: "pi pi-fw pi-list",
+        styleClass: "top-menu",
+        routerLink: `/${FConstants.EDI_LIST}`,
+      },
+      {
+        label: "menu-4.sub-menu.sub-2",
+        visible: true,
+        icon: "pi pi-fw pi-history",
+        styleClass: "top-menu",
+        routerLink: `/${FConstants.EDI_DUE_DATE}`,
+      },
+      {
+        label: "menu-4.sub-menu.sub-3",
+        visible: true,
+        icon: "pi pi-fw pi-file-check",
+        styleClass: "top-menu",
+        routerLink: `/${FConstants.EDI_APPLY_DATE}`,
+      }
+    ]
+  }
+}
