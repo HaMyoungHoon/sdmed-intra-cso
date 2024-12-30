e3e91f23a52592fe07277a50413c29bcdd8a1133
HaMyoungHoon <basicouts@gmail.com>
2024-12-30 14:38:40 +0900

    qna-view 추가

--- src/app/components/app-main/qna/qna-view/qna-view.component.ts
+++ src/app/components/app-main/qna/qna-view/qna-view.component.ts
@@ -1,5 +1,20 @@
-import { Component } from "@angular/core";
+import {Component, ElementRef, input, ViewChild} from "@angular/core";
 import {FComponentBase} from "../../../../guards/f-component-base";
+import {RequestModel} from "../../../../models/rest/requst/request-model";
+import {QnaListService} from "../../../../services/rest/qna-list.service";
+import {UserRole} from "../../../../models/rest/user/user-role";
+import {ActivatedRoute} from "@angular/router";
+import * as FExtensions from "../../../../guards/f-extensions";
+import * as FConstants from "../../../../guards/f-constants";
+import {QnAHeaderModel} from "../../../../models/rest/qna/qna-header-model";
+import {QnAContentModel} from "../../../../models/rest/qna/qna-content-model";
+import {DashboardService} from "../../../../services/rest/dashboard.service";
+import {QnAState, QnAStateToQnAStateDesc} from "../../../../models/rest/qna/qna-state";
+import {QnAFileModel} from "../../../../models/rest/qna/qna-file-model";
+import {transformToBoolean} from "primeng/utils";
+import {QnAReplyModel} from "../../../../models/rest/qna/qna-reply-model";
+import {EditorTextChangeEvent} from "primeng/editor";
+import {UploadFileBuffModel} from "../../../../models/common/upload-file-buff-model";
 
 @Component({
   selector: "app-qna-view",
@@ -8,7 +23,230 @@ import {FComponentBase} from "../../../../guards/f-component-base";
   standalone: false,
 })
 export class QnaViewComponent extends FComponentBase {
-  constructor() {
-    super();
+  @ViewChild("inputFiles") inputFiles!: ElementRef<HTMLInputElement>;
+  thisPK: string;
+  requestModel?: RequestModel;
+  qnaHeaderModel: QnAHeaderModel = new QnAHeaderModel();
+  qnaContentModel: QnAContentModel = new QnAContentModel();
+  qnaReplyModel: QnAReplyModel = new QnAReplyModel();
+  accordionValue = ["0"];
+  content: string = "";
+  htmlValue: string = "";
+  saveAble: boolean = false;
+  uploadFileBuffModel: UploadFileBuffModel[] = [];
+  activeIndex: number = 0;
+  constructor(private thisService: QnaListService, private dashboardService: DashboardService, private route: ActivatedRoute) {
+    super(Array<UserRole>(UserRole.Admin, UserRole.CsoAdmin, UserRole.Employee));
+    this.thisPK = this.route.snapshot.params["thisPK"];
   }
+
+  override async ngInit(): Promise<void> {
+    await this.refreshData();
+  }
+  layoutInit(): void {
+    this.qnaReplyModel = new QnAReplyModel();
+    this.content = "";
+    this.uploadFileBuffModel = [];
+  }
+
+  async refreshData(): Promise<void> {
+    this.setLoading();
+    this.layoutInit();
+    await this.getReqModel();
+    await this.getHeader();
+    this.setLoading(false);
+  }
+
+  async getReqModel(): Promise<void> {
+    const ret = await FExtensions.restTry(async() => this.dashboardService.getRequestData(this.thisPK),
+      e => this.fDialogService.error("getReqModel", e));
+    if (ret.result) {
+      this.requestModel = ret.data;
+      return;
+    }
+    this.fDialogService.warn("getReqModel", ret.msg);
+  }
+  async getHeader(): Promise<void> {
+    const ret = await FExtensions.restTry(async() => await this.thisService.getHeaderData(this.thisPK),
+      e => this.fDialogService.error("getHeader", e));
+    if (ret.result) {
+      this.qnaHeaderModel = ret.data ?? new QnAHeaderModel();
+      await this.getContent();
+      return;
+    }
+    this.fDialogService.warn("getHeader", ret.msg);
+  }
+  async getContent(): Promise<void> {
+    const ret = await FExtensions.restTry(async() => await this.thisService.getContentData(this.thisPK),
+      e => this.fDialogService.error("getContent", e));
+    if (ret.result) {
+      this.qnaContentModel = ret.data ?? new QnAContentModel();
+      this.accordionValue = [`${this.qnaContentModel.replyList.length - 1}`];
+      console.log(this.qnaContentModel.replyList);
+      return;
+    }
+    this.fDialogService.warn("getContent", ret.msg);
+  }
+
+  async downloadFile(item: QnAContentModel): Promise<void> {
+
+  }
+
+  getBlobUrl(item: QnAFileModel): string {
+    const ext = FExtensions.getExtMimeType(item.mimeType);
+    if (FExtensions.isImage(ext)) {
+      return item.blobUrl;
+    }
+    return FExtensions.extToBlobUrl(ext);
+  }
+
+  get canReply(): boolean {
+    return this.qnaHeaderModel.qnaState != QnAState.OK;
+  }
+  async saveReplyData(): Promise<void> {
+    this.qnaReplyModel = new QnAReplyModel();
+    this.qnaReplyModel.content = this.htmlValue;
+    this.setLoading();
+    let fileRet = await this.uploadAzure();
+    if (!fileRet) {
+      this.setLoading(false);
+      return;
+    }
+
+    let uploadRet = await this.qnaUpload();
+    if (!uploadRet) {
+      this.setLoading(false);
+      return;
+    }
+    this.uploadFileBuffModel.forEach(x => x.revokeBLob());
+
+    this.setLoading(false);
+  }
+  async uploadAzure(): Promise<boolean> {
+    const thisPK = this.requestModel?.requestItemPK;
+    if (thisPK == null) {
+      return false;
+    }
+
+    let ret = true;
+    for (const buff of this.uploadFileBuffModel) {
+      const qnaFileModel = FExtensions.getQnAReplyPostFileModel(buff.file!!, thisPK, buff.ext, buff.mimeType);
+      const sasKey = await FExtensions.restTry(async() => await this.commonService.getGenerateSas(qnaFileModel.blobName),
+        e => this.fDialogService.error("saveData", e));
+      if (!sasKey.result) {
+        this.fDialogService.warn("saveData", sasKey.msg);
+        ret = false;
+        break;
+      }
+      await FExtensions.tryCatchAsync(async() => await this.azureBlobService.putUpload(buff.file!!, qnaFileModel.blobName, sasKey.data ?? "", qnaFileModel.mimeType),
+        e => {
+          this.fDialogService.error("saveData", e);
+          ret = false;
+        });
+      if (!ret) {
+        break;
+      }
+      this.qnaReplyModel.fileList.push(qnaFileModel);
+    }
+
+    return ret;
+  }
+  async qnaUpload(): Promise<boolean> {
+    const thisPK = this.requestModel?.requestItemPK;
+    if (thisPK == null) {
+      return false;
+    }
+
+    const ret = await FExtensions.restTry(async() => await this.thisService.postReply(thisPK, this.qnaReplyModel),
+      e => this.fDialogService.error("saveData", e));
+    if (ret.result) {
+      await this.refreshData();
+      return true;
+    }
+
+    this.fDialogService.warn("saveData", ret.msg);
+    return false;
+  }
+
+
+  quillOnInit(_: any): void {
+    const quill = document.getElementById("quillEditor")?.getElementsByClassName("ql-image");
+    if (quill == undefined || quill.length <= 0) {
+      return;
+    }
+    quill[0].remove();
+  }
+  editorChange(data: EditorTextChangeEvent): void {
+    this.htmlValue = data.htmlValue;
+    this.checkSavable();
+  }
+  checkSavable(): void {
+    if (this.htmlValue.length <= 0) {
+      this.saveAble = false;
+      return;
+    }
+
+    this.saveAble = true;
+  }
+  async fileUpload(): Promise<void> {
+    this.inputFiles.nativeElement.click();
+  }
+  async fileSelected(data: Event): Promise<void> {
+    const input = data.target as HTMLInputElement;
+    if (input.files && input.files.length > 0) {
+      this.setLoading();
+      const gatheringFile = (await FExtensions.gatheringAbleFile(input.files, (file: File): void => {
+        this.translateService.get("common-desc.not-supported-file").subscribe(x => {
+          this.fDialogService.warn("fileUpload", `${file.name} ${x}`);
+        });
+      })).filter(x => !!x.file);
+      if (gatheringFile.length > 0) {
+        this.uploadFileBuffModel = this.uploadFileBuffModel.concat(gatheringFile);
+        this.uploadFileBuffModel = FExtensions.distinctByFields(this.uploadFileBuffModel, ["file.name", "file.size"]);
+      }
+      this.inputFiles.nativeElement.value = "";
+      this.checkSavable();
+
+      this.setLoading(false);
+    }
+  }
+
+  get acceptFiles(): string {
+    return ".jpg,.jpeg,.png,.webp,.bmp,.xlsx,.pdf,.heif,.heic,.gif";
+  }
+
+  deleteUploadFile(data: UploadFileBuffModel): void {
+    const index = this.uploadFileBuffModel.indexOf(data);
+    if (index == this.uploadFileBuffModel.length - 1) {
+      if (this.uploadFileBuffModel.length - 1 > 0) {
+        this.activeIndex = this.uploadFileBuffModel.length - 2;
+      } else {
+        this.activeIndex = 0;
+      }
+    }
+
+    if (index >= 0) {
+      data.revokeBLob();
+      this.uploadFileBuffModel.splice(index, 1);
+    }
+  }
+
+  multipleEnable = input(true, { transform: (v: any) => transformToBoolean(v) });
+  accordionIndex(item: QnAReplyModel): string {
+    return `${this.qnaContentModel.replyList.findIndex(x => x.thisPK == item.thisPK)}`;
+  }
+
+  get downloadFileTooltip(): string {
+    return "common-desc.save";
+  }
+  get removeFileTooltip(): string {
+    return "common-desc.remove";
+  }
+
+  protected readonly dateToYYYYMMdd = FExtensions.dateToYYYYMMdd;
+  protected readonly getQnAStateSeverity = FExtensions.getQnAStateSeverity;
+  protected readonly ellipsis = FExtensions.ellipsis;
+  protected readonly QnAStateToQnAStateDesc = QnAStateToQnAStateDesc;
+
+  protected readonly galleriaContainerStyle = FConstants.galleriaContainerStyle;
 }
--- src/app/components/app-main/qna/qna-view/qna-view.module.ts
+++ src/app/components/app-main/qna/qna-view/qna-view.module.ts
@@ -1,14 +1,26 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import {QnaViewComponent} from "./qna-view.component";
-import {QnaListRoutingModule} from "../qna-list/qna-list-routing.module";
+import {Accordion, AccordionContent, AccordionHeader, AccordionPanel} from "primeng/accordion";
+import {Button} from "primeng/button";
+import {Card} from "primeng/card";
+import {Editor} from "primeng/editor";
+import {GalleriaModule} from "primeng/galleria";
+import {PrimeTemplate} from "primeng/api";
+import {ProgressSpinComponent} from "../../../common/progress-spin/progress-spin.component";
+import {SafeHtmlPipe} from "../../../../guards/safe-html.pipe";
+import {Tag} from "primeng/tag";
+import {Tooltip} from "primeng/tooltip";
+import {TranslatePipe} from "@ngx-translate/core";
+import {QnaViewRoutingModule} from "./qna-view-routing.module";
+import {FormsModule} from "@angular/forms";
 
 
 
 @NgModule({
   declarations: [QnaViewComponent],
   imports: [
-    CommonModule, QnaListRoutingModule
+    CommonModule, QnaViewRoutingModule, Accordion, AccordionContent, AccordionHeader, AccordionPanel, Button, Card, Editor, GalleriaModule, PrimeTemplate, ProgressSpinComponent, SafeHtmlPipe, Tag, Tooltip, TranslatePipe, FormsModule
   ]
 })
 export class QnaViewModule { }
