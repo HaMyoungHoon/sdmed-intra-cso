37c5f86454a62eeb76320b349138fb6f74c825c2
하명훈 <basicouts@gmail.com>
2025-03-20 20:03:24 +0900

    약품 데이터베이스 변경

--- src/app/models/rest/medicine/medicine-model.ts
+++ src/app/models/rest/medicine/medicine-model.ts
@@ -24,36 +24,42 @@ export class MedicineModel {
 
   init(data: MedicineModel): MedicineModel {
     this.thisPK = data.thisPK;
-    this.code = data.code;
-    this.mainIngredientCode = data.mainIngredientCode;
-    this.kdCode = data.kdCode;
-    this.clientName = data.clientName ?? "";
-    this.makerName = data.makerName ?? "";
-    this.makerCode = data.makerCode;
+    this.makerName = data.makerName;
     this.orgName = data.orgName;
     this.innerName = data.innerName;
+    this.kdCode = data.kdCode;
     this.customPrice = data.customPrice;
     this.charge = data.charge;
-    this.maxPrice = data.maxPrice;
+    this.standard = data.standard;
+    this.etc1 = data.etc1;
+    this.mainIngredientCode = data.mainIngredientCode;
+    this.code = data.code;
+    this.makerCode = data.makerCode;
+    this.medicineDiv = data.medicineDiv;
     this.inVisible = data.inVisible;
+    this.clientName = data.clientName;
+    this.maxPrice = data.maxPrice;
     this.medicineIngredientModel = data.medicineIngredientModel;
     this.medicinePriceModel = data.medicinePriceModel;
     return this;
   }
   copyLhsFromRhs(lhs: MedicineModel, rhs: MedicineModel): void {
     lhs.thisPK = rhs.thisPK;
-    lhs.code = rhs.code;
-    lhs.mainIngredientCode = rhs.mainIngredientCode;
-    lhs.kdCode = rhs.kdCode;
-    lhs.clientName = rhs.clientName ?? "";
-    lhs.makerName = rhs.makerName ?? "";
-    lhs.makerCode = rhs.makerCode;
+    lhs.makerName = rhs.makerName;
     lhs.orgName = rhs.orgName;
     lhs.innerName = rhs.innerName;
+    lhs.kdCode = rhs.kdCode;
     lhs.customPrice = rhs.customPrice;
     lhs.charge = rhs.charge;
-    lhs.maxPrice = rhs.maxPrice;
+    lhs.standard = rhs.standard;
+    lhs.etc1 = rhs.etc1;
+    lhs.mainIngredientCode = rhs.mainIngredientCode;
+    lhs.code = rhs.code;
+    lhs.makerCode = rhs.makerCode;
+    lhs.medicineDiv = rhs.medicineDiv;
     lhs.inVisible = rhs.inVisible;
+    lhs.clientName = rhs.clientName;
+    lhs.maxPrice = rhs.maxPrice;
     lhs.medicineIngredientModel = rhs.medicineIngredientModel;
     lhs.medicinePriceModel = rhs.medicinePriceModel;
   }
